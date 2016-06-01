fb5e7ce0f2f914705c6ef845e42e238c5799e3bb
Antoine Lefebvre-Brossard <antoinelb7@gmail.com>
2016-06-01 13:17:47 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dynamic Memory Network using TensorFlow
+
+This is based on the paper by Kumar (2015) and the implementation by [YerevaNN](https://github.com/YerevaNN/Dynamic-memory-networks-in-Theano)
---
+++ dmn.py
@@ -0,0 +1,24 @@
+import numpy as np
+import tensorflow as tf
+
+
+class DMN(object):
+    """Dynamic Memory Network (Kumar 2015)."""
+
+    def __init__(self):
+
+        ####################
+        ### Input module ###
+        ####################
+
+        #######################
+        ### Question module ###
+        #######################
+
+        ##############################
+        ### Episodic memory module ###
+        ##############################
+
+        #####################
+        ### Answer module ###
+        #####################
